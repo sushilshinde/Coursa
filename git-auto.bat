3df0db57c6ada415c207ef6d23eb147c20e7ed23
Sushil From Win10 <shinde.sushil@gmail.com>
2017-06-27 16:37:48 +0530

    Commiting

--- git-auto.bat
+++ git-auto.bat
@@ -1,5 +1,5 @@
 git pull
 git add -A
 git commit -m "Commiting"
-git remote add origin https://github.com/sushilshinde/Coursa.git
+REM git remote add origin https://github.com/sushilshinde/Coursa.git
 git push -u origin master
---
+++ git-bb-auto.sh
@@ -0,0 +1,5 @@
+git pull
+git add -A
+git commit -m "Commiting"
+#git remote add BBCoursa https://sshinde@bitbucket.org/sshinde/coursa.git
+git push -u BBCoursa master
