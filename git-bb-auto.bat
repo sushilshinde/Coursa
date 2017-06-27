63555cf92629f6a47de6f5ecd25a0f52269a4ab5
Sushil From Win10 <shinde.sushil@gmail.com>
2017-06-27 16:38:15 +0530

    Commiting

--- git-bb-auto.bat
+++ git-bb-auto.bat
@@ -1,5 +1,5 @@
 git pull
 git add -A
 git commit -m "Commiting"
-REM git remote add BBCoursa https://sshinde@bitbucket.org/sshinde/coursa.git
+git remote add BBCoursa https://sshinde@bitbucket.org/sshinde/coursa.git
 git push -u BBCoursa master
